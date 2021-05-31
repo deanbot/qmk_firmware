0c40c5b1950bb4d59f1297563c0c8297bb325d7b
Dean Verleger <deanverleger@gmail.com>
2021-05-31 15:42:34 -0500

    add ferris layout

---
+++ keyboards/ferris/keymaps/deanbot/rules.mk
@@ -0,0 +1,18 @@
+# BOOTLOADER = atmel-dfu
+
+TAP_DANCE_ENABLE = false
+
+MOUSEKEY_ENABLE = yes
+
+RGBLIGHT_ENABLE = yes
+
+COMBO_ENABLE = yes
+
+# reduce filesize
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+EXTRAFLAGS += -flto
+
+SRC += mousekeys.c
+SRC += swapper.c
+SRC += oneshot.c
